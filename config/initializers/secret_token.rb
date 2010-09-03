b3bc1bfe8e5b768db4e944b143f394f9eb4d5e08
Orient <orient@orient-pc.(none)>
2010-09-04 01:04:22 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '6f4cf0857c3d2105e67258ff4aa525f29f21a9e04cc52427fe42f9b890c66bfa16818a8210a4e41984c3755718b74a98419c9fcb0d6c18d2edcea54fc1bd23ce'
